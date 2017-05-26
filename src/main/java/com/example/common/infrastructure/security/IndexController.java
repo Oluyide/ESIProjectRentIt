aa8bdf1e6cc1856f023b13c634b13f8b2de3fc73
Geraldine Granada <gkgranada@gmail.com>
2017-05-26 11:21:55 +0300

    Add login screen

--- src/main/java/com/example/common/infrastructure/security/IndexController.java
+++ src/main/java/com/example/common/infrastructure/security/IndexController.java
@@ -2,6 +2,7 @@ package com.example.common.infrastructure.security;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController implements org.springframework.boot.autoconfigure.web.ErrorController{
 
+
+    @GetMapping("/login")
+    public String getLoginForm(Model model) {
+        return "dashboard/login";
+    }
 
     // Login form with error
     @RequestMapping("/error")
